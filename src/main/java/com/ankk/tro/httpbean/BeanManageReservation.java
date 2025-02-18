23c7e6110e62a68dcccf285298e63482ca727f30
Arnaud Koffi N GBANDAMA <angbanda@groupement.systeme-u.fr>
2025-02-18 09:58:31 +0100

    Ajout du BEAN

---
+++ src/main/java/com/ankk/tro/httpbean/BeanManageReservation.java
@@ -0,0 +1,13 @@
+package com.ankk.tro.httpbean;
+
+import lombok.Data;
+
+@Data
+public class BeanManageReservation {
+    private long id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String nationnalite;
+    private String channelid;
+}
